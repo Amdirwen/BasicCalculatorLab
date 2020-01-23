946e1c1decc01f8b45f85816d6243fec6039f22e
Amdirwen <59981866+Amdirwen@users.noreply.github.com>
2020-01-23 16:47:39 -0600

    added dewpoint and windchill
    added the dewpoint and windchill calculations

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,19 +6,30 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC DEW POINT CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter the air temperature (in degrees farenheit)");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int temperature = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the relative humidity");
+            int relativeHumidity = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            int dewPoint = temperature - 9*(100-relativeHumidity)/25;
 
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("The dew point is " + dewPoint);
+
+            Console.WriteLine("Enter the wind speed in mph");
+            int windSpeed = int.Parse(Console.ReadLine());
+
+            double result;
+            double exponent = 0.16;
+            result = Math.Pow(windSpeed, exponent);
+
+            double windChill = 35.75 + 0.6215*temperature - 35.75*result + 0.4275*temperature*result;
+
+            Console.WriteLine("The wind chill is " + windChill);
         }
     }
 }
